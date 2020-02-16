4d738038ab1f5b1dd88dea02651c076316fde634
Terje Tjervaag <terje@tjervaag.com>
2020-02-16 20:07:31 +0100

    Simple styling

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,10 +4,9 @@ import { VisibilityFilters } from '../modules/todo/constants'
 
 const Filters = () => (
   <p>
-    Show: <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
-    {", "}
+    Show:
+    <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
     <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
-    {", "}
     <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
   </p>
 );
