0320d834b239051545c54e07c0aa3c8a1a26339b
Terje Tjervaag <terje@tjervaag.com>
2020-02-17 19:51:15 +0100

    Feature/redux tests (#11)
    * Unit tests
    
    * Todo tests
    
    * Fix (?) app test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from '../redux/configureStore';
 import App from '../pages/App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const store = configureStore();
+  const { getByText } = render(<Provider store={store}><App /></Provider>);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
